824007543575f29db187c97c3e3da33d10a8c9cd
github username <joshmrallen@gmail.com>
2020-07-15 11:20:44 -0400

    added writers and max/min for happiness & hygiene

--- lib/person.rb
+++ lib/person.rb
@@ -3,21 +3,32 @@ require 'pry'
 # your code goes here
 class Person
     #each instance of Person should be able to remember their name
-    attr_accessor :name, :bank_account, :happiness
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
 
-    def initialize(name, bank_account = 25, happiness = 8)
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
         @name = name
         @bank_account = bank_account
         @happiness = happiness
+        @hygiene = hygiene
     end
 
     # def bank_account
 
     # end
 
+    def happiness=(num)
+        @happiness = num.clamp(0, 10)
+    end
+
+    def hygiene=(num)
+        @hygiene = num.clamp(0, 10)
+    end
+
+
 
 
 end
 
 person1 = Person.new("Ian")
-binding.pry
+# binding.pry
