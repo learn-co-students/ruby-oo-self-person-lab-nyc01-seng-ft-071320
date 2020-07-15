a18aa4eae913af988750227e83c26e0d971fb465
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-15 11:40:45 -0400

    Peer Coding Challenge 1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,55 @@
 # your code goes here
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+def initialize(name)
+@name = name
+@bank_account = 25
+@happiness = 8
+@hygiene = 8
+end
+
+def happiness
+   if @happiness > 10
+    @happiness = 10
+   elsif @happiness < 0
+    @happiness = 0
+   else 
+    @happiness
+   end
+end
+
+def hygiene
+    if @hygiene > 10
+        @hygiene = 10
+    elsif @hygiene < 0
+        @hygiene = 0
+    else
+        @hygiene
+    end
+end
+
+
+def happy? 
+    if @happiness > 7
+        true
+    else 
+        false
+    end
+end
+def clean?
+   if @hygiene > 7
+    true
+   else
+    false
+   end
+end
+
+def get_paid (salary)
+    @bank_account += salary
+"all about the benjamins"
+end
+
+
+end
