908c33f6c928117c4c82e3b6e47763c91f9fb1f9
Mimioj101 <mimioj@gmail.com>
2020-07-15 10:46:38 -0400

    Initializes name and bank_account

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 # your code goes here
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+    end
+
+end
