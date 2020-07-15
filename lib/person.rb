2acbc4e3affafc22ea07bdd6d4214a54c9f589dc
victorscholz <vgscholz@gmail.com>
2020-07-15 11:06:26 -0400

    work in progress

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
 # your code goes here
+
+class Person
+    attr_accessor :bank_account, :happiness
+    attr_reader :name
+
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        # @hygiene = hygiene
+        # bank_account = 25, happiness, hygiene
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness
+        @happiness = (0..10)
+        # 0..10 happiness can't go below 0 or over 10
+    end
+
+    
+    
+
+
+
+
+
+
+end
