cf80798fee1f30d161a49ad3411fcf5ed39272d2
irosen419 <irosen419@gmail.com>
2020-07-15 10:55:13 -0400

    initialized class Person with name, bank and happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
+require 'pry'
+
 # your code goes here
+class Person
+    #each instance of Person should be able to remember their name
+    attr_accessor :name, :bank_account, :happiness
+
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+    end
+
+    # def bank_account
+
+    # end
+
+
+
+end
+
+person1 = Person.new("Ian")
+binding.pry
