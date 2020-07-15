03d00b50391f4b1ce55b29dcb2993242175438a7
“Mineliegoma” <garlandegoma@gmail.com>
2020-07-15 15:24:27 -0400

    test command

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,53 @@
 # your code goes here
 class Person
     attr_accessor :bank_account
-
+    attr_accessor :happiness
+    attr_accessor :hygiene
+    @@all=[]
     def initialize(name)
         @bank_account = 25
+         @happiness = 8
+         @hygiene = 8
         @name = name
+        @@all << self 
 
     end
-#def methods
+
+
+
     def name
         @name
     end
+    def happiness
+    @happiness.clamp(0,10)
+    end 
+    def hygiene
+        @hygiene.clamp(0,10)
+    end 
+    def happy? 
+     if @happiness > 7
+        true
+     else
+        false
+     end 
+    end 
+    def clean?
+        if @hygiene > 7 
+            true
+        else 
+            false
+        end 
+    end 
+    def get_paid(salary)
+        @bank_account += salary
+    
+        'all about the benjamins'
 
+    end 
+    def take_bath
+       @hygiene=8
+       '♪ Rub-a-dub just relaxing in the tub ♫'
+       
+
+    end 
 end
