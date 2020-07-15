7a684f1239f939b5f4f185ecb452000e4c00bfa7
junko911 <junkotahara911@gmail.com>
2020-07-15 11:16:30 -0400

    Made happiness method

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,20 @@
 class Person
-   attr_reader :name
+   attr_reader :name, :happiness
+   attr_accessor :bank_account
 
     def initialize(name)
         @name = name 
         @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end 
 
+    def happiness=(happiness)
+        @happiness = happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+    end
 end 
