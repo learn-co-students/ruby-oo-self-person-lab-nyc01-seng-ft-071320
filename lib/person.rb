281102ec82a27efec57a1b1489366d7384867aec
brianwlego <legobw@gmail.com>
2020-07-15 11:01:38 -0400

    Add happiness and hygiene methods

--- lib/person.rb
+++ lib/person.rb
@@ -2,10 +2,34 @@
 
 class Person
     attr_reader :name
-    attr_accessor :bank_account
+    attr_accessor :bank_account, :happiness, :hygiene
     def initialize(name)
         @name = name
         @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
+
+    def happiness
+      if @happiness > 10
+        @happiness = 10
+      elsif @happiness.between?(0,10)
+        @happiness
+      else
+        @happiness < 0
+        @happiness = 0
+      end
+    end
+    def hygiene
+      if @hygiene > 10
+        @hygiene = 10
+      elsif @hygiene.between?(0,10)
+        @hygiene
+      else
+        @hygiene < 0
+        @hygiene = 0
+      end
+    end
+
 
 end
