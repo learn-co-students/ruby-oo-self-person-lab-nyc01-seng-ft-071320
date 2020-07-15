5f0840dab490dee2d9cc9670f3d4792388e90d84
stackguardian <mferdous98@aol.com>
2020-07-15 11:35:30 -0400

    bank account balance

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @bank_account = 25
+        @name = name
+
+    end
+
+    def name
+        @name
+    end
+
+end
