ae529fe2715afef2b2fe82a00a04966854fbb354
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-07-15 16:35:06 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 class Person
     attr_accessor :bank_account, :happiness, :hygiene
@@ -31,8 +31,48 @@ class Person
             @hygiene
         end
     end
-#binding.pry
 
-see ya later!
-enjoy lunch
-end
+    def happy?
+        @happiness > 7 
+    end 
+    
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics" 
+            self.happiness -= 2
+            friend.happiness -= 2
+                return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+                return "blah blah sun blah rain"
+        else 
+            return "blah blah blah blah blah"
+        end
+    end
+end 
