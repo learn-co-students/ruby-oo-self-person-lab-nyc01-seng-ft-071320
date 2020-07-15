cecc259fac5a00ad68f5a4a3c3d046be7a34e0d5
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-15 11:33:30 -0400

    max/min set

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,35 @@
 # your code goes here
 class Person
 
-    attr_reader :name
-    attr_accessor :happiness, :bank_account, :hygiene 
+    attr_reader :name, :hygiene
+    attr_accessor :happiness, :bank_account  
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else @hygiene = hygiene
+        end
+        @hygiene
+    end
+
+    def happiness=(happiness)
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else @happiness = happiness
+        end
+        @happiness
+    end
+
     
-    def initialize(name)
+    def initialize(name, hygiene=8, happiness=8)
         @name = name
         @bank_account = 25
-        @happiness = 8
-        @hygiene = 8 
+        @happiness = happiness
+        @hygiene = hygiene
     end
 
     def happy?
@@ -33,11 +54,13 @@ class Person
     end
 
     def take_bath
-        @hygiene += 4
+        self.hygiene+=4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end 
 
-
-    
-
+    def work_out
+        self.happiness+=2
+        self.hygiene-=3
+        "♪ another one bites the dust ♫"
+    end
 end 
