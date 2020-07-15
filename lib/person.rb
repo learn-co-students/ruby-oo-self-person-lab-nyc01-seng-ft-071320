ef781ce0b05bfe455925b3ad1e6b9d9fa3abc2e3
JMHero05 <jake.mills.05@gmail.com>
2020-07-15 12:10:37 -0400

    Almost Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,30 +1,78 @@
 # your code goes here
 
 class Person
-    attr_accessor :bank_account, :happiness
-    attr_reader :name
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+    attr_writer 
 
-    def initialize(name, bank_account = 25, happiness = 8)
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
         @name = name
         @bank_account = bank_account
         @happiness = happiness
-        # @hygiene = hygiene
+        @hygiene = hygiene
         # bank_account = 25, happiness, hygiene
     end
 
     def happiness=(happiness)
         @happiness = happiness
-        # happiness.include?  
-        # @happiness = (0..10).include?(happiness)
-        # 0..10 happiness can't go below 0 or over 10
+            if @happiness > 10
+                @happiness = 10
+            elsif @happiness < 0
+                @happiness = 0
+            end
     end
 
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+            if @hygiene > 10
+                @hygiene = 10
+            elsif @hygiene < 0
+                @hygiene = 0
+            end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
     
-    
+    def clean?
+        if @hygiene > 7
+            true
+        else 
+            false
+        end
+    end
 
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
 
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
 
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
 
+    def call_friend(name)
+        self.happiness += 3
+        name.happiness += 3
+        "Hi #{name.name}! It's #{self.name}. How are you?"
+    end
 
-
+    def start_conversation(name, topic)
+        if topic == 'politics'
+            
+            "blah blah partisan blah lobbyist"
+        end
+    end
 end
