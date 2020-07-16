b131e9b0db1893174df6898affa62ece7cdf37d4
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-16 11:48:17 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -52,4 +52,37 @@ def get_paid (salary)
 end
 
 
+def take_bath 
+    self.hygiene = @hygiene + 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+end
+
+def work_out
+    self.hygiene = @hygiene - 3
+    self.happiness = @happiness + 2
+    "♪ another one bites the dust ♫"
+end
+
+def call_friend (friend)
+    self.happiness = @happiness + 3
+    friend.happiness +=3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+end
+
+
+def start_conversation (person, topic)
+    if topic == "politics"
+        self.happiness = @happiness - 2
+        person.happiness -= 2
+"blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+        self.happiness = @happiness + 1
+        person.happiness += 1
+        "blah blah sun blah rain"
+    else 
+        "blah blah blah blah blah"
+    end
+end
+
+
 end
